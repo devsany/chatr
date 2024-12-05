842c7d22e10ea13ad98c811bffc4bda1dbda8cee
devsany <sunnyrajbodhgaya19@gmail.com>
2024-12-05 12:20:32 +0530

    update the Ui and functionality to transfer the data from NavLink (to conversation checking the functionality

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,10 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore/lite";
-import { getDatabase } from "firebase/database";
-#import { getDatabase, ref, get } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,8 +21,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-const db = getDatabase(app); // Initialize Realtime Database
 
-export { db, ref, get };
 export default app;
